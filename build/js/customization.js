1efd566cba019b5d54988fcf97ed0e6d3a2ef529
Dmitry Kostrubiak <dminked@gmail.com>
2016-10-15 19:48:26 +0300

    import repo from bitbucket to github

--- build/js/customization.js
+++ build/js/customization.js
@@ -1,6 +1,6 @@
 ;'use strict';
 
-// Main navigation
+// Main navigation - mobile
 
     $(function() {
 
@@ -20,6 +20,58 @@
             }
         });
 
+    });
+
+// Main navigation - moving to anchor and active item
+
+    $(document).ready(function () {
+        $(document).on("scroll", onScroll);
+
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
+
+            $('a').each(function () {
+                $(this).removeClass('active');
+            });
+            $(this).addClass('active');
+
+            var target = this.hash,
+                menu = target;
+            $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top - 60 + 'px'
+            }, 500, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        });
+    });
+
+    function onScroll(event){
+        var scrollPos = $(document).scrollTop();
+
+        $('.js-main-menu a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+
+            if (refElement.position().top-60 <= scrollPos && refElement.position().top-60 + refElement.height() > scrollPos) {
+                $('.js-main-menu li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else {
+                currLink.removeClass("active");
+            }
+        });
+    }
+
+    $(window).scroll(function () {
+
+        if  ($(window).scrollTop() == $(document).height() - $(window).height()) {
+            $('.js-main-menu li:last-child a').addClass('active');
+            $('.js-menu-link-penult').removeClass('active');
+        }
     });
 
 // Slider promo
